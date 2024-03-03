99ffcbb9edd0e9cad4f6a49eea49a4a7a26665d8
Marco Antonio <marcoalvesce@gmail.com>
2024-03-03 19:13:31 -0300

    segundo commit

---
+++ Introdução a algoritmos/Pesquisa Binaria/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Diagnostics.Tracing;
+using System.Runtime.CompilerServices;
+namespace PesquisaBinaria
+{
+    public class Program
+    {
+        static int pesquisar(int[] lista, int item)
+        {
+            int baixo = 0;
+            int alto = lista.Length - 1;
+
+            while (baixo <= alto)
+            {
+                int meio = (baixo + alto)/2;
+                int chute = lista[meio];
+                if (chute == item)
+                    return meio;
+                if (chute > item)
+                    alto = meio - 1;
+                else
+                {
+                    baixo = meio + 1;
+                }
+            }
+            return -1;
+        }
+        static void Main(string [] args){
+            int[] minha_lista = [1, 3, 5, 7, 9];
+
+            Console.WriteLine(pesquisar(minha_lista, 1));
+            Console.WriteLine(pesquisar(minha_lista, 3));
+            Console.WriteLine(pesquisar(minha_lista, 5));
+            Console.WriteLine(pesquisar(minha_lista, 7));
+            Console.WriteLine(pesquisar(minha_lista, 9));
+            Console.WriteLine(pesquisar(minha_lista, 2));
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+Esse projeto é um estudo de algoritmos baseado no livro Entendendo Algoritmos do Aditya Y. Bhargava
+
+Exercicios - Capitulo 1:
+    1.1: log²128 = 7, logo, 7 etapas.
+    1.2: 8 etapas
